4be8249ab0dd3aed2b659e01f3164f231cc4f256
o81018o <102898398+o81018o@users.noreply.github.com>
2022-05-02 10:57:20 +0800

    Add files via upload

---
+++ 易查分/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(788, 340);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "启  动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "运行中断";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "当前条目：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(342, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "接收数量：";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(479, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 21);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 424);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "易查分批量查询";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ 易查分/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Fleck;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StreamReader streamReader;
+        StreamReader streamReader_;
+        StreamWriter streamWriter;
+        WebSocketServer server;
+        WebSocketServer server_;
+        int index = 1,count = 0;
+        bool ig = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(ig)
+            {
+                server.Dispose();
+                server_.Dispose();
+                ig = false;
+                button1.Text = "启  动";
+                label1.Text = "运行中断";
+                streamWriter.Close();
+                streamReader.Close();
+                streamReader_.Close();
+                streamReader.Dispose();
+                streamReader_.Dispose();
+                streamWriter.Dispose();
+                return;
+            }
+            streamReader = new StreamReader(@"d://1.txt");
+            streamReader_ = new StreamReader(@"d://2.txt");
+            streamWriter = new StreamWriter(@"d://3.txt", false, Encoding.UTF8);
+            server = new WebSocketServer("ws://0.0.0.0:7181");
+            server_ = new WebSocketServer("ws://0.0.0.0:7182");
+            count = 0;
+            index = ((int)numericUpDown1.Value);
+            for (int i = 0;i < index - 1; i++)
+            {
+                streamReader.ReadLine();
+                streamReader_.ReadLine();
+            }
+            FleckLog.Level = LogLevel.Debug;
+            server.Start(socket =>
+            {
+                socket.OnMessage = str =>
+                {
+                    Invoke(new Action(() => { listBox1.Items.Add(str); listBox1.SelectedIndex = listBox1.Items.Count - 1; }));
+                    streamWriter.WriteLine(str);
+                    count++;
+                    Invoke(new Action(() => { label3.Text = "接收数量：" + count; }));
+                };
+            });
+            server_.Start(socket =>
+            {
+                socket.OnMessage = str =>
+                {
+                    var line = streamReader.ReadLine();
+                    line += ' ';
+                    line += streamReader_.ReadLine();
+                    socket.Send(line);
+                    index++;
+                    Invoke(new Action(() => { label2.Text = "当前条目：" + index; }));
+                };
+            });
+            button1.Text = "停  止";
+            label1.Text = "正在运行";
+            ig = true;
+        }
+    }
+}
